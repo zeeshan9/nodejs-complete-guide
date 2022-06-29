9d3f42ebadf645e035e2d2572f14ccc1b8d5496d
zeeshan9 <zeeshanmushtaq76@gmail.com>
2022-06-29 13:30:34 +0500

    installed mysql2

--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,27 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 // const rootDir = require('../util/path');
-const productsController = require('../controllers/products')
+// const productsController = require('../controllers/products')
+const shopController = require('../controllers/shop');
 
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
+
+router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
+
+// router.get('/', productsController.getProducts);
 
 router.get('/test', (req, res) => {
     /** __dirname refer to the current directory in which we are in and it works well both on linux system and windows system*/
